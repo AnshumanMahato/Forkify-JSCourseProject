228f84da6799af88baa8bce61995806008cab3b9
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-02-26 10:35:27 +0530

    init commit

---
+++ src/js/controller.js
@@ -0,0 +1,14 @@
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+// api key : 4cf4ee42-6ec3-45a9-bf25-940ad9eb0083
+
+///////////////////////////////////////
