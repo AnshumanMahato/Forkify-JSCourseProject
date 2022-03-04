3a7a71bedd522bb60f7b576dd426bf92f94a08ae
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-03-04 21:54:41 +0530

    polyfilling solutions

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,8 @@
 //jshint esversion:8
 import icons from 'url:../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -17,8 +20,22 @@ console.log('Built');
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const markup = `
+  <div class="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div>
+        `;
+  parentEl.innerHTML='';
+  parentEl.insertAdjacentHTML('afterbegin',markup);
+};
+
 const showRecipe = async function () {
   try {
+
+    renderSpinner(recipeContainer);
     // Loading recipe
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
