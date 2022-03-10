815d276f7ffa27b636ac4a1472a460c0e5f3360e
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-03-10 21:17:11 +0530

    update servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -60,8 +60,17 @@ const controlPagination = function(gotoPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  //Update servings in state
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
