db9e9acd4ea25754fb38412eb02acac1e2cd4407
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-03-14 14:42:40 +0530

    add recipe init

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,44 @@
+import View from "./View.js";
+
+class AddRecipeView extends View {
+    _parentElement = document.querySelector('.upload');
+
+    _overlay = document.querySelector('.overlay');
+    _window = document.querySelector('.add-recipe-window');
+
+    _btnOpen = document.querySelector('.nav__btn--add-recipe');
+    _btnClose = document.querySelector('.btn--close-modal');
+
+    _toggleWindowState() {
+        this._overlay.classList.toggle('hidden');
+        this._window.classList.toggle('hidden');
+    }
+
+    _addHandlerOpen() {
+        this._btnOpen.addEventListener('click',this._toggleWindowState.bind(this));
+    }
+
+    _addHandlerClose() {
+        this._btnClose.addEventListener('click',this._toggleWindowState.bind(this));
+        this._overlay.addEventListener('click',this._toggleWindowState.bind(this));
+    }
+
+    addHandlerSubmit(handler) {
+        this._parentElement.addEventListener('submit', function(e) {
+            e.preventDefault();
+            const rawData = [...new FormData(this)];
+            const data = Object.fromEntries(rawData);
+
+            handler(data);
+        })
+    }
+
+    constructor() {
+        super();
+        this._addHandlerClose();
+        this._addHandlerOpen();
+    }
+
+}
+
+export default new AddRecipeView();
