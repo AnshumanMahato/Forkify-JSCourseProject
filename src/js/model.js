aecc00bad2ab37e1f1417bc288187a25bcfefc00
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-03-14 15:26:50 +0530

    ingredient formatting

--- src/js/model.js
+++ src/js/model.js
@@ -99,16 +99,22 @@ export const deleteBookmark = function (id) {
 };
 
 export const uploadRecipe = async function (newRecipe) {
-  const ingredients = Object.entries(newRecipe).filter(
-    entry => entry[0].startsWith('ingredient') && entry[1] !== ''
-  ).map(ing => {
-    const ingData = ing[1].split(',');
-    if(ingData.length < 3) 
-      throw new Error('Wrong ingredient format!!! Please use the correct ingredient format :)');
-    const [quantity,unit,description] = ingData;
+  const ingredients = Object.entries(newRecipe)
+    .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
+    .map(ing => {
+      const ingData = ing[1].split(',');
+      if (ingData.length < 3)
+        throw new Error(
+          'Wrong ingredient format!!! Please use the correct ingredient format :)'
+        );
+      const [quantity, unit, description] = ingData;
 
-    return {quantity: quantity!== ''?+quantity:null,unit:unit.trim(),description: description.trim()};
-  });
+      return {
+        quantity: quantity !== '' ? +quantity : null,
+        unit: unit.trim(),
+        description: description.trim(),
+      };
+    });
 
   console.log(ingredients);
 };
